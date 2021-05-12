ec27373ba77543d553c3c81404423f13aa5b7431
Alex Bento <melo1bento@gmail.com>
2021-05-12 13:29:18 -0300

    Aplicativo de tarefas, podendo remover tarefas , adicionar tarfas e ver o seu progresso

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+		<TasksProgress :progress="progress" />
+		<NewTask  @taskAdded="addTask"/>
+		<TaskGrid
+			:tasks="tasks"
+			@taskDeleted="deleteTask"
+			@taskStateChanged="toogleTaskState" />
+	</div>
+</template>
+
+<script>
+import TasksProgress from './components/TasksProgress'
+import NewTask from './components/NewTask'
+import TaskGrid from './components/TaskGrid.vue'
+
+export default {
+	components:  {TasksProgress, NewTask, TaskGrid  },
+	data(){
+		return{
+			tasks: []
+		}
+	},
+	computed:{
+		progress(){
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
+		}
+	},
+	watch:{
+		tasks: {
+			deep: true, 
+			handler(){
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
+	methods: {
+		addTask(task){
+
+			if(!task.name.trim()){return alert('Por favor, preencha uma Tarefa')}
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			
+
+			if(reallyNew){
+				this.tasks.push({
+					name: task.name,
+					pending: task.pending || true
+				})
+			}
+		},
+		deleteTask(indice){
+			this.tasks.splice(indice, 1)
+		},
+		toogleTaskState(indice){
+			this.tasks[indice].pending = !this.tasks[indice].pending
+		}
+	},
+	created(){
+		const json = localStorage.getItem('tasks')
+		const array = JSON.parse(json)
+		
+		this.tasks = Array.isArray(array) ? array : []
+
+		
+	}
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Lato', sans-serif;
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		color: #FFF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+</style>
